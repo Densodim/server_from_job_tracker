d598aa04c439f4b47be042ed55cfc1f40c360742
Densodim <25denso@gmail.com>
2024-12-04 12:41:22 +0300

    env

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from 'cors';
+import cors from "cors";
 import bodyParser from "body-parser";
-import {router} from "./routes/jobRoutes.js";
-import dotenv from 'dotenv';
-
+import { router } from "./routes/jobRoutes.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -13,23 +12,29 @@ const port = process.env.PORT || 5000;
 const dbUri = process.env.MONGO_PUBLIC_URL;
 
 if (!dbUri) {
-    console.error("Ошибка: Строка подключения не найдена!");
-    process.exit(1);
+  console.error("Ошибка: Строка подключения не найдена!");
+  process.exit(1);
 }
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 app.use(router);
 
 mongoose
-    .connect(dbUri)
-    .then(() => {
-        console.log('Подключение к базе данных успешно');
-        app.listen(port, () => {
-            console.log(`Сервер запущен на порту ${port}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Ошибка при подключении к базе данных:', error);
+  .connect(dbUri)
+  .then(() => {
+    console.log("Подключение к базе данных успешно");
+    app.listen(port, () => {
+      console.log(`Сервер запущен на порту ${port}`);
     });
+  })
+  .catch((error) => {
+    console.error("Ошибка при подключении к базе данных:", error);
+  });
