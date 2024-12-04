682e9175c02bf0db3c224713feb7da7f436e27c9
Densodim <25denso@gmail.com>
2024-12-04 22:23:05 +0300

    add cookparser

--- api/authMiddleware.js
+++ api/authMiddleware.js
@@ -1,19 +1,15 @@
-import jwt from "jsonwebtoken";
-
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies._vercel_jwt; // Получаем куку
-
-  if (!token) {
-    return res.status(401).json({ message: "Неавторизован" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.API_TOKEN); // Проверяем токен
-    req.user = decoded; // Сохраняем данные пользователя
-    next(); // Переходим к следующему middleware
-  } catch (error) {
-    return res.status(403).json({ message: "Доступ запрещен" });
-  }
-};
-
-export default authMiddleware;
+   const token = req.cookies._vercel_jwt; // Получаем куку
+ 
+   if (!token) {
+     // Если токен отсутствует, просто продолжаем выполнение
+     console.log("Кука отсутствует, но продолжаем выполнение");
+   } else {
+     // Вы можете здесь просто сохранить токен в `req`, если нужно
+     req.token = token;
+   }
+ 
+   next(); // Переходим к следующему middleware
+ };
+ 
+ export default authMiddleware;
