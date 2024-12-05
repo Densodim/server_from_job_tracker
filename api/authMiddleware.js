f41749ed9f8579c1417262ce55133449c52c3c46
Densodim <25denso@gmail.com>
2024-12-05 23:39:15 +0300

    delete comments

--- api/authMiddleware.js
+++ api/authMiddleware.js
@@ -1,15 +1,13 @@
 const authMiddleware = (req, res, next) => {
-   const token = req.cookies._vercel_jwt; // Получаем куку
- 
-   if (!token) {
-     // Если токен отсутствует, просто продолжаем выполнение
-     console.log("Кука отсутствует, но продолжаем выполнение");
-   } else {
-     // Вы можете здесь просто сохранить токен в `req`, если нужно
-     req.token = token;
-   }
- 
-   next(); // Переходим к следующему middleware
- };
- 
- export default authMiddleware;
+  const token = req.cookies._vercel_jwt;
+
+  if (!token) {
+    console.log("Кука отсутствует, но продолжаем выполнение");
+  } else {
+    req.token = token;
+  }
+
+  next();
+};
+
+export default authMiddleware;
